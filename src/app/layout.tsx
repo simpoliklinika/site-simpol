e7266937ccf3bf655001115140ebfd6d585b7d1c
Ilya Khurtak <ilyakhurtak@MacBook-Pro-2.local>
2025-08-22 08:39:41 +0300

    pushed layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,8 +54,9 @@ export const metadata: Metadata = {
   icons: {
     // файли мають існувати у /public
     icon: [
-      { url: "/icon-32.png", sizes: "32x32", type: "image/png" },
-      { url: "/icon-192.png", sizes: "192x192", type: "image/png" }, // фавікон для вкладки/маніфест
+      { url: "/favicon.ico" },
+      { url: "icons/icon-192.png", sizes: "192x192", type: "image/png" },
+      { url: "icons/icon-512.png", sizes: "512x512", type: "image/png" }, // фавікон для вкладки/маніфест
     ],
     apple: [
       { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
